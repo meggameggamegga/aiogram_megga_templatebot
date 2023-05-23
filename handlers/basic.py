89f8a59dc10e09e924223f8e997e28ac60e9a149
meggameggamegga <megga.faceit@mail.ru>
2023-05-23 15:13:02 +0300

    my commit

--- handlers/basic.py
+++ handlers/basic.py
@@ -1,1 +1,12 @@
+from aiogram import types
+from aiogram.dispatcher.filters import Command
+
 from main import bot,dp
+
+
+
+
+
+@dp.message_handler(Command('start'))
+async def start_cmnd(message:types.Message):
+    pass
